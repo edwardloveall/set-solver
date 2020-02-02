48d8c59f38a5aaac6a83af1eef4e8676fdf8b14e
Edward Loveall <edward@edwardloveall.com>
2020-02-02 15:29:30 -0500

    Show board and solutions

--- src/Main.elm
+++ src/Main.elm
@@ -8,11 +8,12 @@ import Card
         , CountAttribute(..)
         , FillAttribute(..)
         , ShapeAttribute(..)
+        , cardNeededForSet
         , displayedCard
         )
-import Html exposing (Html, section)
+import Html exposing (Html, div, h2, p, section, text)
 import Html.Attributes exposing (class)
-import List.Extra
+import List.Extra exposing (uniquePairs)
 import Random
 import Random.List exposing (shuffle)
 
@@ -44,6 +45,16 @@ initialModel _ =
     ( [], Random.generate NewBoard (shuffle allCards) )
 
 
+allCards : List Card
+allCards =
+    List.Extra.lift4
+        Card
+        [ Single, Double, Triple ]
+        [ Red, Green, Purple ]
+        [ Empty, Striped, Filled ]
+        [ Diamond, Oval, Squiggle ]
+
+
 
 -- UPDATE
 
@@ -56,7 +67,7 @@ update : Msg -> Model -> ( Model, Cmd msg )
 update msg _ =
     case msg of
         NewBoard cards ->
-            ( cards, Cmd.none )
+            ( List.take 12 cards, Cmd.none )
 
 
 
@@ -65,23 +76,37 @@ update msg _ =
 
 view : Model -> Html msg
 view model =
-    section [ class "wrapper" ]
-        (List.map
-            displayedCard
-            (randomBoard model)
-        )
+    div [ class "wrapper" ]
+        [ h2 [] [ text "board" ]
+        , section
+            [ class "board" ]
+            (List.map
+                displayedCard
+                model
+            )
+        , h2 [] [ text "solutions" ]
+        , List.map (List.map displayedCard) (solutions model) |> List.map (p []) |> section [ class "solutions" ]
+        ]
 
 
-randomBoard : Model -> List Card
-randomBoard model =
-    List.take 12 model
+solutions : Model -> List (List Card)
+solutions cards =
+    List.map (fullMatch cards) (uniquePairs cards)
 
 
-allCards : List Card
-allCards =
-    List.Extra.lift4
-        Card
-        [ Single, Double, Triple ]
-        [ Red, Green, Purple ]
-        [ Empty, Striped, Filled ]
-        [ Diamond, Oval, Squiggle ]
+fullMatch : Model -> ( Card, Card ) -> List Card
+fullMatch cards ( card1, card2 ) =
+    let
+        match =
+            cardNeededForSet card1 card2
+    in
+    if matchExistsOnBoard cards match then
+        [ card1, card2, match ]
+
+    else
+        []
+
+
+matchExistsOnBoard : Model -> Card -> Bool
+matchExistsOnBoard cards match =
+    List.member match cards
